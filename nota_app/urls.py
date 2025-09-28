3a1ece539a2302519e0f004f34111190a5c443d5
Ciro <romano.agustin24@gmail.com>
2025-09-28 14:24:50 -0300

    edicion de notas activas

--- nota_app/urls.py
+++ nota_app/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [ 
+
     path('', views.lista_notas, name='lista_notas'), 
     path('crear_nota/', views.crear_notas, name='crear_nota'),
+    path('nota/<int:pk>/editar/', views.editar_notas, name='editar_nota'),
     path('eliminar/<int:id>/', views.eliminar_notas, name='eliminar_nota'),
-] 
+    
+    ]
--- nota_app/views.py
+++ nota_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404,redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Nota
 from .forms import NotaForm
 
@@ -6,29 +6,50 @@ from .forms import NotaForm
 def lista_notas(request):
 
     notas = Nota.objects.all()
-    return render(request, 'nota_app/home.html', {'notas': notas} )
+
+    return render(request, 'nota_app/lista_notas.html', {'notas': notas} )
 
 def crear_notas(request):
 
     if request.method == 'POST':
+
         form = NotaForm(request.POST)
 
         if form.is_valid():
+
             form.save()
 
             return redirect('lista_notas')
         
     else:
+
         form = NotaForm()
+
     return render(request, 'nota_app/form_nota.html', {'form': form} )
-        
+
+def editar_notas(request, pk):
+    
+    nota = get_object_or_404(Nota, pk=pk)
+
+    if request.method == 'POST':
+
+        form = NotaForm(request.POST, instance=nota)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect('lista_notas')
+    else:
+
+        form = NotaForm(instance=nota)
+
+    return render(request, 'nota_app/editar_nota.html', {'form': form} )
+
 def eliminar_notas(request, id):
 
     nota = get_object_or_404(Nota, id=id)
+
     nota.delete()
+
     return redirect('lista_notas')
-
-
-
-
-
